435d7e0ed60b44648054cd50464bce4ec8f26526
Yirmerah <jeremy.dormevil@gmail.com>
2021-11-29 15:03:47 +0100

    pt11+12

--- app/src/main/java/com/jrmydorm/todo/tasklist/TaskListAdapter.kt
+++ app/src/main/java/com/jrmydorm/todo/tasklist/TaskListAdapter.kt
@@ -1,42 +1,34 @@
 package com.jrmydorm.todo.tasklist
-
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
-import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
-import com.jrmydorm.todo.R
+import com.jrmydorm.todo.databinding.ItemTaskBinding
 import com.jrmydorm.todo.model.Task
 
 object TasksDiffCallback : DiffUtil.ItemCallback<Task>() {
     override fun areItemsTheSame(oldItem: Task, newItem: Task) =  oldItem.id == newItem.id;
-    override fun areContentsTheSame(oldItem: Task, newItem: Task) =oldItem.equals(newItem);
+    override fun areContentsTheSame(oldItem: Task, newItem: Task) = oldItem.equals(newItem);
 }
 
 
 class TaskListAdapter() : ListAdapter<Task,TaskListAdapter.TaskViewHolder>(TasksDiffCallback) {
 
-    inner class TaskViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    inner class TaskViewHolder(private val binding: ItemTaskBinding) : RecyclerView.ViewHolder(binding.root) {
         fun bind(task: Task) {
-            itemView.findViewById<TextView>(R.id.task_title).text = task.title
-            itemView.findViewById<TextView>(R.id.task_desc).text = task.description
+            binding.taskTitle.text =  task.title
+            binding.taskDesc.text = task.description
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
-        val itemView = LayoutInflater.from(parent.context).inflate(
-            R.layout.item_task,
-            parent,
-            false
-        )
-        return TaskViewHolder(itemView)
-
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ItemTaskBinding.inflate(inflater, parent, false)
+        return TaskViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
         holder.bind(currentList[position])
-
     }
 }
--- app/src/main/java/com/jrmydorm/todo/tasklist/TaskListFragment.kt
+++ app/src/main/java/com/jrmydorm/todo/tasklist/TaskListFragment.kt
@@ -6,13 +6,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.jrmydorm.todo.R
+import com.jrmydorm.todo.databinding.FragmentTaskListBinding
 import com.jrmydorm.todo.model.Task
 import java.util.*
 
 class TaskListFragment : Fragment() {
+    private lateinit var binding: FragmentTaskListBinding
+
     private val taskList = mutableListOf(
         Task(id = "id_1", title = "Task 1", description = "description 1"),
         Task(id = "id_2", title = "Task 2"),
@@ -23,19 +23,19 @@ class TaskListFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val rootView = inflater.inflate(R.layout.fragment_task_list, container, false)
-        return rootView
+        super.onCreateView(inflater, container, savedInstanceState)
+        binding = FragmentTaskListBinding.inflate(layoutInflater)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val recyclerView = view.findViewById<RecyclerView>(R.id.recycler_view)
+        val recyclerView = binding.recyclerView
         recyclerView.layoutManager = LinearLayoutManager(activity)
         val adapter = TaskListAdapter()
-        recyclerView.adapter = adapter
+        binding.recyclerView.adapter = adapter
         adapter.submitList(taskList.toList())
-        val button = view.findViewById<FloatingActionButton>(R.id.floatingActionButton);
-        button.setOnClickListener {
+        binding.floatingActionButton.setOnClickListener{
             taskList.add(Task(id = UUID.randomUUID().toString(), title = "Task ${taskList.size + 1}"))
             recyclerView.adapter = adapter
             adapter.submitList(taskList.toList())

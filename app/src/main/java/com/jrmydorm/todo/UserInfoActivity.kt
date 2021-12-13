da36a74f01627a1f5d760db7b658bea8beef1507
Yirmerah <jeremy.dormevil@gmail.com>
2021-12-13 15:43:35 +0100

    TP5.PT3

--- app/src/main/java/com/jrmydorm/todo/UserInfoActivity.kt
+++ app/src/main/java/com/jrmydorm/todo/UserInfoActivity.kt
@@ -1,11 +1,77 @@
 package com.jrmydorm.todo
 
+import android.Manifest
+import android.app.AlertDialog
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.widget.Button
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContentProviderCompat.requireContext
 
 class UserInfoActivity : AppCompatActivity() {
+
+    private fun showExplanation() {
+        // ici on construit une pop-up système (Dialog) pour expliquer la nécessité de la demande de permission
+        AlertDialog.Builder(this)
+            .setMessage("🥺 On a besoin de la caméra, vraiment! 👉👈")
+            .setPositiveButton("Bon, ok") { _, _ ->  launchAppSettings() }
+            .setNegativeButton("Nope") { dialog, _ -> dialog.dismiss() }
+            .show()
+    }
+
+    private fun launchCameraWithPermission() {
+        val camPermission = Manifest.permission.CAMERA
+        val permissionStatus = checkSelfPermission(camPermission)
+        val isAlreadyAccepted = permissionStatus == PackageManager.PERMISSION_GRANTED
+        val isExplanationNeeded = shouldShowRequestPermissionRationale(camPermission)
+        when {
+            isAlreadyAccepted -> launchCamera()
+            isExplanationNeeded -> showExplanation()
+            else -> cameraPermissionLauncher.launch(camPermission)
+        }
+    }
+
+
+    private val cameraPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { accepted ->
+            if (accepted) {
+                // lancer l'action souhaitée
+                launchCamera()
+            }
+            else {
+                // afficher une explication
+                showExplanation()
+            }
+        }
+
+
+    private fun handleImage(imageUri: Uri) {
+        // afficher l'image dans l'ImageView
+    }
+
+    private fun launchCamera() {
+        // à compléter à l'étape suivante
+    }
+
+    private fun launchAppSettings() {
+        val intent = Intent(
+            Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+            Uri.fromParts("package", this.packageName, null)
+        )
+        startActivity(intent)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_info)
+        val take_picture_button =  findViewById<Button>(R.id.take_picture_button);
+        take_picture_button.setOnClickListener{
+            launchCameraWithPermission()
+        }
+
     }
 }
